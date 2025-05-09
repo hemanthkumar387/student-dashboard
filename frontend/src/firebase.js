82a7276d60a94ad94d7527789a8ee0404f8b829b
hemanthkumar387 <hemanthnamala387@gmail.com>
2025-05-09 12:36:05 +0530

    first commit

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -1,16 +1,11 @@
-// Replace with your Firebase config
-import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+ import { initializeApp } from "firebase/app";
+   import { getAuth } from "firebase/auth";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBhw6mGvdEvH5U9i4EkrgVTmidubgzfqGQ",
-    authDomain: "studentdashboard-bc472.firebaseapp.com",
-    projectId: "studentdashboard-bc472",
-    storageBucket: "studentdashboard-bc472.firebasestorage.app",
-    messagingSenderId: "791852182703",
-    appId: "1:791852182703:web:db1ec2a3408ec78d43f85a",
-    measurementId: "G-5QB0H68S57"
-};
+   const firebaseConfig = {
+     apiKey: "YOUR_API_KEY",
+     authDomain: "YOUR_AUTH_DOMAIN",
+     projectId: "YOUR_PROJECT_ID",
+   };
 
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+   const app = initializeApp(firebaseConfig);
+   export const auth = getAuth(app);
